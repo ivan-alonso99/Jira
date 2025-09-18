ed20688122325b551f9c89ee93d5137d441fd6bb
ivan-alonso99 <ivan.a.h.99.15@gmail.com>
2025-09-17 21:41:06 -0600

    DragsAndDrop

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let draggedElement = null;
 
-function openModal()   {
+function openModal() {
     const modal = document.getElementById('modal');
     modal.showModal();
 }
-function agregarTarea()     {
+function agregarTarea(event) {
+    event.preventDefault();
     const titulo = document.getElementById('title-task').value;
     const descripcion = document.getElementById('description').value;
     const toDo = document.getElementById('to-do-list');
@@ -11,18 +13,33 @@ function agregarTarea()     {
     const newDiv = document.createElement('div');
     newDiv.classList.add('newDiv');
     newDiv.innerText = titulo;
-
+    newDiv.setAttribute('draggable', 'true');
     newDiv.innerHTML = `
     <p>${descripcion}</p>
      <h2>${titulo}</h2>
-    `;    
+    `;
 
-    const tareas = [];
-    
+    newDiv.addEventListener('dragstart', function (event) {
+        draggedElement = event.target;
+    });
+
     toDo.appendChild(newDiv);
 }
-function closeModal(event)   {
+
+function closeModal(event) {
     event.preventDefault();
     const modal = document.getElementById('modal');
-    modal.close();    
+    modal.close();
+}
+
+
+function arrastrar(event) {
+    event.preventDefault();
+}
+
+function soltar(event) {
+    event.preventDefault();
+    if (draggedElement) {
+        event.currentTarget.appendChild(draggedElement);
+    }
 }
