a9a8677ad4dc582bf6706f2a86918b7c9663f311
irvingivangutierrezviveros-netizen <irvingivangutierrezviveros@gmail.com>
2025-09-18 00:31:11 -0600

    agregue estilos css y funciones en JS

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ function openModal() {
     const modal = document.getElementById('modal');
     modal.showModal();
 }
+
 function agregarTarea(event) {
     event.preventDefault();
+    const error= document.getElementById('error');
     const titulo = document.getElementById('title-task').value;
     const descripcion = document.getElementById('description').value;
     const toDo = document.getElementById('to-do-list');
+
+    if(titulo === '' || descripcion === ''){
+        error.innerText= "Llena todos los campos"
+        setTimeout(() => {
+            error.innerText= '';
+        }, 5000);
+    } else{
 
     const newDiv = document.createElement('div');
     newDiv.classList.add('newDiv');
@@ -17,6 +26,9 @@ function agregarTarea(event) {
     newDiv.innerHTML = `
     <p>${descripcion}</p>
      <h2>${titulo}</h2>
+     <button class="trash" onclick="deleteTask(this)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+</svg></button>
     `;
 
     newDiv.addEventListener('dragstart', function (event) {
@@ -24,7 +36,18 @@ function agregarTarea(event) {
     });
 
     toDo.appendChild(newDiv);
+
+
+    document.getElementById("title-task").value = '';
+    document.getElementById("description").value = '';
 }
+}
+
+    function deleteTask(button){
+        const task = button.parentElement;
+        task.remove();
+    }
+
 
 function closeModal(event) {
     event.preventDefault();
